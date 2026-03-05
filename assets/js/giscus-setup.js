f6ff3168d9080517884417fdd5f7f0f0bacc436f
Chaos-Q <pohoulong668@gmail.com>
2026-03-05 08:05:45 +0000

    Deploying to gh-pages from @ Chaos-Q/Chaos-Q.github.io@44874969ed163ef1ab8a6f71a2f3294af3e98738 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Chaos-Q/website",
+    "data-repo": "Chaos-Q/Chaos-Q.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
